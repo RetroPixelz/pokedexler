e3f64b5912ca2dffbc75496617ca468485e4d80b
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-03-24 12:21:11 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import Battle from "./components/Battle";
 import Header from "./components/Header";
 import Dex from "./components/Dex"
 import { Outlet, Link } from "react-router-dom";
-// import Info from "./components/Info"
 import {Routes, Route} from 'react-router-dom';
 
 
 export default function App() {
   return (
-    <div className="logo">
+    <>
       
       <nav className="nav">
 
-        <div className="LOGO"></div>
-      
-        <Link to="/Dex">Dex</Link> 
-        <Link to="/Battle">Battle</Link>
-      
+        <ul>
+          <li>
+          <div className="LOGO"></div>
+            <Link to="/Dex"><h2>Dex</h2></Link> 
+            <Link to="/Battle"><h2>Battle</h2></Link>
+          </li>   
+        </ul>
         
       </nav>
+
+      <div className="Back">
+        
+      </div>
       <Outlet />
-    </div>
+    </>
   );
 }
 
