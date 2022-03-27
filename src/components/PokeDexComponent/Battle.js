b9eb4d771a5495b7ae5bd6b5701c0d7f787f2b06
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-03-27 14:07:49 +0300

    added images adn styling
    i added another image to generate a pokeball in my battle ground page and also placed the cards and started styling them to spec

--- src/components/PokeDexComponent/Battle.js
+++ src/components/PokeDexComponent/Battle.js
@@ -5,7 +5,28 @@ export default function Battle(){
     return (
         <>
         <div className="battleground">
-            
+            <div className="row-1">
+
+            </div>
+            <div className="flex-container">
+                <div className="flex-item">
+                    <div className="pokemon-1"></div>
+                    <div className="stats-1"></div>
+                </div>
+                <div className="flex-item" id="fight">
+                    
+                    <div className="vs"></div>
+                    <h2> Fight! </h2>
+                </div>
+                <div className="flex-item">
+                    <div className="pokemon-2"></div>
+                    <div className="stats-2"></div>
+                </div>
+
+            </div>
+            <div className="row-3">
+
+            </div>
         </div>
         </>
     )
