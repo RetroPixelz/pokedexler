d2ef76ff7dbd0ec1433bc01cd2768ec93f9e282e
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-04-08 21:16:38 +0300

    Update Battle.js

--- src/components/Header/Main/Battle/Battle.js
+++ src/components/Header/Main/Battle/Battle.js
@@ -69,7 +69,7 @@ export default function Battle({ pokemonDataBaseNamesList, pokemonDataBase }) {
   //       backgroundColor: "rgba(255, 99, 132, 0.2)",
   //       borderColor: "rgba(255, 99, 132, 1)",
   //       borderWidth: 1,
-  //     },
+  //     }, 
   //   ],
   // }; 
 
